4e1cce8daabab87dac8084d54d588e02b2842e7c
Kemal04 <kemalhojayew04@gmail.com>
2024-05-05 17:45:34 +0500

    responsive-home-page

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,63 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top py-3" style={{ background: "#131417" }}>
-            <div className="container">
-                <div className="w-100 text-white d-flex justify-content-between">
-                    <Link to="/" className="nav-link">BIZ BARADA</Link>
-                    <Link to="/" className="nav-link">MENU</Link>
-                    <Link to="/" className="nav-link">RESERWASIÝA</Link>
+        <>
+
+            <nav className="navbar navbar-expand-lg navbar-dark fixed-top py-3" style={{ background: "#131417" }}>
+                <div className="container">
+                    <div className="w-100 text-white d-xl-flex justify-content-between d-none">
+                        <Link to="/" className="nav-link">BIZ BARADA</Link>
+                        <Link to="/" className="nav-link">MENU</Link>
+                        <Link to="/" className="nav-link">RESERWASIÝA</Link>
+                    </div>
+                    <div className="px-5 mx-3 text-center" style={{ width: "1000px" }}>
+                        <Link to="/" className="fw-semibold text-white text-decoration-none h5 me-xl-0 me-4">Tamdyr Lezzeti</Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                    </div>
+                    <div className="w-100 text-white d-xl-flex justify-content-between d-none">
+                        <Link to="/" className="nav-link">TÄZELIKLER</Link>
+                        <Link to="/" className="nav-link">IŞ SAGATLARYMYZ</Link>
+                        <Link to="/" className="nav-link">HABARLAŞMAK</Link>
+                    </div>
                 </div>
-                <div className="px-5 mx-3 text-center" style={{ width: "1000px" }}>
-                    <Link to="/" className="fw-semibold text-white text-decoration-none h5">Tamdyr Lezzeti</Link>
-                </div>
-                <div className="w-100 text-white d-flex justify-content-between">
-                    <Link to="/" className="nav-link">TÄZELIKLER</Link>
-                    <Link to="/" className="nav-link">IŞ SAGATLARYMYZ</Link>
-                    <Link to="/" className="nav-link">HABARLAŞMAK</Link>
+            </nav >
+
+            <div className={`offcanvas offcanvas-start navbar-dark`} style={{ width: "270px", background: "#131417" }} data-bs-scroll="true" tabIndex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                <div className="offcanvas-header flex-column align-items-start p-0">
+
+                    <div className='d-flex align-items-center justify-content-between p-3'>
+                        <h5 className="offcanvas-title me-1 pe-5 text-white" id="offcanvasWithBothOptionsLabel">Tamdyr Lezzeti</h5>
+                        <button type="button" className={`btn-close ms-4`} data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+
+                    <div className="d-flex flex-column mt-3">
+                        <ul className="navbar-nav p-3">
+                            <li className="nav-item mx-xl-2 mx-lg-2 mx-md-2 mx-sm-0 mx-0" style={{ fontSize: "17px" }}>
+                                <NavLink to="/" className="nav-link fw-bold">BIZ BARADA</NavLink>
+                            </li>
+                            <li className="nav-item mx-xl-2 mx-lg-2 mx-md-2 mx-sm-0 mx-0" style={{ fontSize: "17px" }}>
+                                <NavLink to="/" className="nav-link fw-bold">MENU</NavLink>
+                            </li>
+                            <li className="nav-item mx-xl-2 mx-lg-2 mx-md-2 mx-sm-0 mx-0" style={{ fontSize: "17px" }}>
+                                <NavLink to="/" className="nav-link fw-bold">RESERWASIÝA</NavLink>
+                            </li>
+                            <li className="nav-item mx-xl-2 mx-lg-2 mx-md-2 mx-sm-0 mx-0" style={{ fontSize: "17px" }}>
+                                <NavLink to="/" className="nav-link fw-bold">TÄZELIKLER</NavLink>
+                            </li>
+                            <li className="nav-item mx-xl-2 mx-lg-2 mx-md-2 mx-sm-0 mx-0" style={{ fontSize: "17px" }}>
+                                <NavLink to="/" className="nav-link fw-bold">IŞ SAGATLARYMYZ</NavLink>
+                            </li>
+                            <li className="nav-item mx-xl-2 mx-lg-2 mx-md-2 mx-sm-0 mx-0" style={{ fontSize: "17px" }}>
+                                <NavLink to="/" className="nav-link fw-bold">HABARLAŞMAK</NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </nav >
+        </>
     )
 }
 
--- src/pages/main/Home.jsx
+++ src/pages/main/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
         <>
             <div className='border-bottom position-relative' style={{ background: "#131417" }}>
                 <div className='container-fluid'>
-                    <div className='row justify-content-center align-items-end' style={{ height: "100dvh" }}>
-                        <div className='col-xl-3'>
+                    <div className='row justify-content-center align-items-end ' style={{ height: "100dvh" }}>
+                        <div className='col-xl-3 d-none d-xl-block d-lg-block d-md-block'>
                             <div className='d-flex flex-column justify-content-end pb-4 '>
                                 <div className='scale border' style={{ width: "200px" }}>
                                     <img src={card5} alt="" className='img-fluid p-2' />
@@ -29,10 +29,10 @@ const Home = () => {
                             </div>
                         </div>
                         <div className='col-xl-4 text-center'>
-                            <div className='display-3 text-white position-relative fw-semibold' style={{ marginBottom: "-50px", zIndex: "1" }}>Tamdyr Lezzetine <br /> Hoş geldiňiz</div>
+                            <div className='display-3 text-white position-relative fw-semibold mb-m-50' style={{ zIndex: "1" }}>Tamdyr Lezzetine <br /> Hoş geldiňiz</div>
                             <img src={card4} alt="" className='img-fluid w-75' style={{ borderRadius: "50% 50% 0 0" }} />
                         </div>
-                        <div className='col-xl-3'>
+                        <div className='col-xl-3 d-none d-xl-block d-lg-block d-md-block'>
                             <div className='d-flex flex-column justify-content-end pb-4 '>
                                 <div className='scale border align-self-end' style={{ width: "200px" }}>
                                     <img src={card7} alt="" className='img-fluid p-2' />
@@ -53,16 +53,16 @@ const Home = () => {
                 <div className="about-home-progress">
                     <div className="about-home-progress-bar"></div>
                 </div>
-                <div className='row align-items-center justify-content-center mt-5'>
-                    <div className='col-xl-5'>
+                <div className='row align-items-center justify-content-end justify-content-xl-center  mt-5'>
+                    <div className='col-xl-5 col-11 order-xl-0 order-1'>
                         <img src={card3} alt="" className="img-fluid" />
                     </div>
-                    <div className="col-xl-1 d-flex justify-content-center h-100">
+                    <div className="col-xl-1 col-1 d-flex justify-content-center h-100">
                         <div className="star-icon">
                             <img src={starIcon} alt="" style={{ width: "1.5rem", height: "1.5rem" }} />
                         </div>
                     </div>
-                    <div className='col-xl-5 text-white'>
+                    <div className='col-xl-5 col-11 order-xl-1 order-0 text-white mb-xl-0 mb-5'>
                         <div className="display-5 mb-4">Biz Barada</div>
                         <div style={{ textAlign: "justify" }}>
                             <b style={{ fontSize: "18px", marginRight: "10px" }}>Tamdyr Lezzeti</b> hakyky türk & türkmen aşhanasynyň iň gowy tagamlaryny hödürleýän restoran. Myhmanlarymyzy mylaýym we täsin atmosferasy bilen öz öýlerinde duýýan Tamdyr tagamynda, adaty peçlerde, täze gök önümlerde we lezzetli işdäaçarlarda bişirilen etler bilen ýatdan çykmajak nahar iýip bilersiňiz.
@@ -70,32 +70,32 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <div className='row align-items-center justify-content-center mt-5'>
-                    <div className='col-xl-5 text-white'>
+                <div className='row align-items-center justify-content-end justify-content-xl-center mt-5'>
+                    <div className='col-xl-5 col-11 order-xl-0 order-1 text-white'>
                         <div className="display-5 mb-4">Aşpezler</div>
-                        <div style={{ textAlign: "justify" }}>
+                        <div className="mb-xl-0 mb-5" style={{ textAlign: "justify" }}>
                             <b style={{ fontSize: "18px", marginRight: "10px" }}>Tamdyr Lezzetiniň</b> aşhanasynda tejribeli we hyjuwly aşpezler topary bar. Aşpezlerimiz türk & türkmen aşhanasynyň adaty reseptlerini iň ownuk jikme-jikliklere çenli bilýärler we bu reseptleri häzirki zaman düşündirişleri bilen garyşdyryp myhmanlarymyza hödürleýärler.
                             <br /><br />
                             Her tagamyň aňyrsynda aşpezlerimiziň yhlasly işi we yhlasy bar. Myhmanlarymyza iň täze ingredientleri ulanmak we her tabak barada alada etmek bilen ýatdan çykmajak nahar iýmek tejribesini hödürlemegi maksat edinýärler.                        </div>
                     </div>
-                    <div className="col-xl-1 d-flex justify-content-center h-100">
+                    <div className="col-xl-1 col-1 order-xl-1 order-0  d-flex justify-content-center h-100">
                         <div className="star-icon">
                             <img src={starIcon} alt="" style={{ width: "1.5rem", height: "1.5rem" }} />
                         </div>
                     </div>
-                    <div className='col-xl-5 position-relative'>
-                        <img src={card2} alt="" className="img-fluid" style={{ borderRadius: "50% 50% 0 0", height: "650px", width: "500px", objectFit: "cover" }} />
-                        <img src={card6} alt="" className='img-fluid position-absolute shadow' style={{ bottom: "7%", left: "55%", width: "450px", height: "280px", objectFit: "cover" }} />
+                    <div className='col-xl-5 col-11 order-xl-2 order-1 position-relative'>
+                        <img src={card2} alt="" className="img-fluid chef-img" />
+                        <img src={card6} alt="" className='img-fluid chef-img-food position-absolute shadow d-none d-xl-block d-lg-block d-md-block ' style={{ bottom: "7%", left: "55%", width: "450px", height: "280px", objectFit: "cover" }} />
                     </div>
                 </div>
-                <div className="star-icon">
+                <div className="star-icon d-none d-xl-block d-lg-block d-md-block ">
                     <img src={starIcon} alt="" style={{ width: "1.5rem", height: "1.5rem" }} />
                 </div>
             </div>
 
             <div className='container py-5' style={{ background: "#131417" }}>
                 <div className='row justify-content-center'>
-                    <div className="col-xl-5">
+                    <div className="col-xl-5 col-10">
                         <Link className='w-100 border rounded-circle d-flex justify-content-center align-items-center position-relative text-white' style={{ padding: "4rem" }}>
                             <div className='position-absolute h1' style={{ zIndex: "3" }}>MENU</div>
                             <img src={roundedText} alt="" className='img-fluid position-absolute w-100 p-3 rounded-text' style={{ zIndex: "2" }} />
@@ -108,24 +108,24 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className='container-fluid mt-5 pt-5 px-0'>
-                <div className='row g-0'>
-                    <div className='col-xl-2'>
+            <div className='container-fluid px-0' style={{ background: "#131417" }}>
+                <div className='row g-0 mt-5 pt-5'>
+                    <div className='col-xl-2 col-6'>
                         <img src={card1} alt="" className='img-fluid' style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                     </div>
-                    <div className='col-xl-2'>
+                    <div className='col-xl-2 col-6'>
                         <img src={card5} alt="" className='img-fluid' style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                     </div>
-                    <div className='col-xl-2'>
+                    <div className='col-xl-2 col-6'>
                         <img src={card3} alt="" className='img-fluid' style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                     </div>
-                    <div className='col-xl-2'>
+                    <div className='col-xl-2 col-6'>
                         <img src={card6} alt="" className='img-fluid' style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                     </div>
-                    <div className='col-xl-2'>
+                    <div className='col-xl-2 col-6'>
                         <img src={card7} alt="" className='img-fluid' style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                     </div>
-                    <div className='col-xl-2'>
+                    <div className='col-xl-2 col-6'>
                         <img src={card5} alt="" className='img-fluid' style={{ width: "100%", height: "250px", objectFit: "cover" }} />
                     </div>
                 </div>
